f152b680aa633b3577f1617f3573ea61c7572215
Eugenio Bergia <48608068+eugeniobergia@users.noreply.github.com>
2019-09-20 13:10:00 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -9,6 +9,8 @@ typedef int (*Comparar) (void *dato1, void *dato2);
 
 typedef void (*Mostrar) (void *dato);
 
+typedef void (*Imprimir) (void *dato, FILE *archivo);
+
 /**************************************************************/
 
 int comparar_judoca(void *dato1, void *dato2) {
@@ -184,7 +186,32 @@ Arbol emparejar(Arbol *equipo1, Arbol *equipo2, Arbol parejas) {
     (*equipo1) = eliminar_nodo((*equipo1), (*equipo1) -> dato, comparar_judoca, destruir_judoca);
     (*equipo2) = eliminar_nodo((*equipo2), (*equipo2) -> dato, comparar_judoca, destruir_judoca);
     return parejas;
+}
 
+void imprimir_judoca(void *dato, FILE *salida) {
+    fprintf(salida, "%s,%s,%d", ((Judoca)dato) -> nombre,((Judoca)dato) -> apellido, ((Judoca)dato) -> edad);
+}
+
+void imprimir_pareja(void *dato, FILE *salida) {
+    imprimir_judoca(((Pareja)dato) -> participante1, salida);
+    fprintf(salida, " - ");
+    imprimir_judoca(((Pareja)dato) -> participante2, salida);
+    fprintf(salida, " - ");
+    if(((Pareja)dato) -> estadoPareja == 0)
+        fprintf(salida, "Pareja valida\n");
+    else if(((Pareja)dato) -> estadoPareja == 1)
+        fprintf(salida, "Pareja de mayor con menor\n");
+    else if(((Pareja)dato) -> estadoPareja == 2)
+        fprintf(salida, "Pareja de menores con diferencia mayor a 1 año\n");
+    else fprintf(salida, "Pareja de mayores con diferencia mayor a 2 años\n");
+}
+
+void imprimir_salida(Arbol arbol, FILE *salida, Imprimir i) {
+    if(arbol) {
+        imprimir_salida(arbol -> izq, salida, i);
+        i(arbol -> dato, salida);
+        imprimir_salida(arbol -> der, salida, i);
+    }
 }
 
 int main(int argc, char **argv) {
@@ -192,8 +219,9 @@ int main(int argc, char **argv) {
         printf("Cantidad de argumentos invalida\n");
         return 0;
     }
-    FILE *entrada;
+    FILE *entrada, *salida;
     entrada = fopen(argv[1], "r");
+    salida = fopen(argv[2], "w");
 
     Arbol equipo1 = NULL;
     Arbol equipo2 = NULL;
@@ -202,10 +230,10 @@ int main(int argc, char **argv) {
     equipo2 = ingresar_equipo2(equipo2, entrada);
 
     Arbol parejas = NULL;
-    for(int i = 0; equipo1 && equipo2; i++)
+    for(; equipo1 && equipo2;)
         parejas = emparejar(&equipo1, &equipo2, parejas);
 
-    mostrar(parejas, mostrar_pareja);
+    imprimir_salida(parejas, salida, imprimir_pareja);
 
     return 0;
 }
